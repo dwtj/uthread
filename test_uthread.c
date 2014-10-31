c41eeda3416c93714381d3a007af86d2b793547f
David Johnston <dj@djohnston.me>
2014-10-31 01:20:52 -0500

    Made tabs in the test consistent.

--- test_uthread.c
+++ test_uthread.c
@@ -11,45 +11,45 @@ sem_t mutex;
 
 void do_something()
 {
-        int id,i,j;
+    int id,i,j;
 
-        sem_wait(&mutex);
+    sem_wait(&mutex);
 
-        id=myid;
-        myid++;
-        printf("This is ult %d\n", id);
+    id=myid;
+    myid++;
+    printf("This is ult %d\n", id);
 
-        if(n_threads<6){
-                n_threads++;
-                sem_post(&mutex);
-                uthread_create(do_something);
-        }else sem_post(&mutex);
+    if(n_threads<6){
+        n_threads++;
+        sem_post(&mutex);
+        uthread_create(do_something);
+    }else sem_post(&mutex);
 
-        if(id%2==0)
-                for(j=0;j<100;j++)
-                        for(i=0;i<1000000;i++) sqrt(i*119.89);
+    if(id%2==0)
+        for(j=0;j<100;j++)
+            for(i=0;i<1000000;i++) sqrt(i*119.89);
 
-        printf("This is ult %d again\n", id);
+    printf("This is ult %d again\n", id);
 
-        uthread_yield();
+    uthread_yield();
 
-        printf("This is ult %d once more\n", id);
+    printf("This is ult %d once more\n", id);
 
-        if(id%2==0)
-                for(j=0;j<100;j++)
-                        for(i=0;i<1000000;i++) sqrt(i*119.89);
+    if(id%2==0)
+        for(j=0;j<100;j++)
+            for(i=0;i<1000000;i++) sqrt(i*119.89);
 
-        printf("This is ult %d exit\n", id);
+    printf("This is ult %d exit\n", id);
 
-        uthread_exit();
+    uthread_exit();
 }
 
 main()
 {
-        int i;
-        system_init(1);
-        setbuf(stdout,NULL);
-        sem_init(&mutex,0,1);
-        uthread_create(do_something);
-        uthread_exit();
+    int i;
+    system_init(1);
+    setbuf(stdout,NULL);
+    sem_init(&mutex,0,1);
+    uthread_create(do_something);
+    uthread_exit();
 }
