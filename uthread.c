bec736361eaba4965a78bfcada6389673a3fc2a7
David Johnston <dj@djohnston.me>
2014-11-02 13:58:45 -0600

    Gave `uthread_t` a running field. Started changes.

--- uthread.c
+++ uthread.c
@@ -34,7 +34,6 @@ typedef struct {
 typedef struct {
 	ucontext_t ucontext;
 	struct timeval running_time;
-	bool active;
 } uthread_t;
 
 
@@ -42,7 +41,7 @@ typedef struct {
 	int tid;
 	struct timeval initial_utime;
 	struct timeval initial_stime;
-	bool active;
+	uthread_t* running;
 } kthread_t;
 
 
@@ -50,14 +49,13 @@ typedef struct {
 /* Declare helper functions. *****************************************************/
 
 int uthread_priority(const void* key1, const void* key2);
-void kthread_init(kthread_t* kt);
 void uthread_init(uthread_t* ut, void (*run_func)());
 int kthread_runner(void* ptr);
+void kthread_timestamp(kthread_t* kt);
 int kthread_create(kthread_t* kt, uthread_t* ut);
 void kthread_handoff(uthread_t* load_from, uthread_t* save_to);
 kthread_t* kthread_self();
 kthread_t* find_inactive_kthread();
-uthread_t* find_inactive_uthread();
 
 
 
@@ -68,8 +66,6 @@ bool _shutdown = false;
 Heap _waiting_uthreads = NULL;
 int _num_kthreads;
 int _max_num_kthreads;
-int _num_uthreads;
-uthread_t* _uthreads;
 kthread_t* _kthreads;
 pthread_mutex_t _mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t _shutdown_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -93,24 +89,19 @@ void uthread_system_init(int max_num_kthreads)
 
 	getcontext(&_system_initializer_context);
 
-	// The highest priority uthread record (i.e. the on with the lowest running time)
-	// will be at top of the `heap`. Thus, the heap is bottom-heavy w.r.t. running time.
+	// The highest priority uthread record (i.e. the on with the lowest running
+	// time) will be at top of the `heap`. Thus, the heap is bottom-heavy w.r.t.
+	// running time.
 	_waiting_uthreads = HEAPinit(uthread_priority, NULL);
 
-	// Allocate memory for each `kthread_t` and mark each as inactive.
+	// Allocate memory for each `kthread_t` and mark each as inactive (i.e. not
+	// running).
 	_kthreads = malloc(_max_num_kthreads * sizeof(kthread_t));
 	for (int i = 0; i < _max_num_kthreads; i++) {
-		_kthreads[i].active = false;
-	}
-
-	// Allocate memory for each `uthread_t` and mark each as inactive.
-	_uthreads = malloc(MAX_NUM_UTHREADS * sizeof(uthread_t));
-	for (int i = 0; i < MAX_NUM_UTHREADS; i++) {
-		_uthreads[i].active = false;
+		_kthreads[i].running = NULL;
 	}
 
 	// Initialize other globals.
-	_num_uthreads = 0;
 	_num_kthreads = 0;
 	_max_num_kthreads = max_num_kthreads;
 	// TODO: initialize `_default_pthread_attr()`.
@@ -131,25 +122,21 @@ int uthread_create(void (*run_func)())
 		pthread_mutex_lock(&_shutdown_mutex);
 	}
 
-	_num_uthreads += 1;
-
-	assert(_num_uthreads < MAX_NUM_UTHREADS);
-	assert(_num_kthreads <= _max_num_kthreads);
-
-	uthread_t* uthread = find_inactive_uthread();
+	uthread_t* uthread = malloc(sizeof(uthread));
+	printf("DEBUG: allocated uthread at %p\n", (void*) uthread);
 	assert (uthread != NULL);
 	uthread_init(uthread, run_func);
 
 	if (_num_kthreads == _max_num_kthreads)
 	{
+		// Add the new `uthread` to the heap.
 		puts("Adding new `uthread` to heap.");
-		// Add the new uthread record to the heap.
 		HEAPinsert(_waiting_uthreads, (const void *) uthread);
 	}
 	else
 	{
+		// Make a new `kthread` to run this new `uthread` immediately.
 		puts("Starting `uthread` on new `kthread`.");
-		// Make a pthread to run this function immediately.
 
 		assert(HEAPsize(_waiting_uthreads) == 0);  // There must not be waiting
 												   // uthreads if `_num_kthreads` is
@@ -176,21 +163,29 @@ void uthread_yield()
 	pthread_mutex_lock(&_mutex);
 	assert(_shutdown == false);
 
+	kthread_t* self = kthread_self();
+	assert(self != NULL);  // Yield cannot be called by a thread that was not
+						   // created by the system.
+
 	if (HEAPsize(_waiting_uthreads) > 0)
 	{
-		// TODO: fix this crazy use of memory!
-		uthread_t* save_to = find_inactive_uthread();
-		save_to->active = true;
+		// Save the current running `uthread` to the heap.
+		uthread_t* prev = self->running;
+		HEAPinsert(_waiting_uthreads, (void *) prev);
 
-		uthread_t* load_from = NULL;
-		HEAPextract(_waiting_uthreads, (void **) &load_from);
-		HEAPinsert(_waiting_uthreads, (void *) save_to);
+		// Get another `uthread` from the heap to be run.
+		uthread_t* next = NULL;
+		HEAPextract(_waiting_uthreads, (void **) &next);
+		self->running = next;
 
-		// TODO: fix this crazy possibility of race conditions!
+		// TODO: consider possibility of race conditions!
 		pthread_mutex_unlock(&_mutex);
-		kthread_handoff(load_from, save_to);
+		kthread_handoff(prev, next);
 	}
-
+	else
+	{
+		pthread_mutex_unlock(&_mutex);
+	}
 }
 
 
@@ -201,6 +196,7 @@ void uthread_exit()
 	pthread_mutex_lock(&_mutex);
 	kthread_t* self = kthread_self();
 	int self_tid;
+	int self_tgid;
 	pthread_mutex_unlock(&_mutex);
 
 	// If the calling thread is not a `kthread` created by the system, block on a
@@ -216,31 +212,39 @@ void uthread_exit()
 	assert(_shutdown == false);
 
 	self = kthread_self();
-	self_tid = self->tid;
+	uthread_t* prev = self->running;
+	uthread_t* next = NULL;
 
-	// Check if a uthread can use this kthread.
+	// TODO: print prev->running_time for debug.
+
+	// Stop running the `prev` `uthread`, and clean up any references to it.
+	self->running = NULL;
+	// TODO: Figure out why this segfaults!
+	//free(prev);
+
+	// Check if a `uthread` can use this kthread.
 	if (HEAPsize(_waiting_uthreads) > 0)
 	{
-		// Use this `kthread`
-		// TODO: fix this crazy use of memory!
-		uthread_t* load_from = NULL;
-		HEAPextract(_waiting_uthreads, (void **) &load_from);
+		// Use this `kthread` to run a different `uthread`.
+		uthread_t* next = NULL;
+		HEAPextract(_waiting_uthreads, (void **) &next);
+		self->running = next;
 
-		// TODO: fix this crazy possibility of race conditions!
+		// TODO: update the `kthread` timestamp.
+		// TODO: update the `uthread` running time.
+
+		// TODO: consider possibility of race conditions.
 		pthread_mutex_unlock(&_mutex);
-		ucontext_t* uc = &(load_from->ucontext);
-		setcontext(uc);
+		setcontext(&(next->ucontext));
 	}
 	else
 	{
-		// There are no `uthread`s that might use `self`.
-
-		// Clean up `uthread`-associated system data structures.
-		// TODO: Follow a link from `kthread` to a `uthread` and what is found there
-		// as inactive.
+		// There are no `uthread`s that might use `self`. Kill `self`.
+		// Find out more about `self` first.
+		self_tid = self->tid;
+		self_tgid = getpid();
 
 		// Clean up `kthread`-associated system data structures.
-		self->active = false;
 		_num_kthreads--;
 
 		// If this was the last `kthread`, then the system-shutdown mutex is unlocked.
@@ -250,7 +254,7 @@ void uthread_exit()
 		}
 
 		pthread_mutex_unlock(&_mutex);
-		tgkill(self_tid, getpid(), SIGKILL);
+		tgkill(self_tid, self_tgid, SIGKILL);
 		assert(false);  // Control should never reach here.
 	}
 }
@@ -259,9 +263,10 @@ void uthread_exit()
 
 /* Define primary helper functions. **********************************************/
 
-void kthread_handoff(uthread_t* load_from, uthread_t* save_to) {
-	assert(load_from != NULL && save_to != NULL);
-	swapcontext(&(save_to->ucontext), &(load_from->ucontext));
+void kthread_handoff(uthread_t* prev, uthread_t* next) {
+	assert(prev != NULL);
+	assert(next != NULL);
+	swapcontext(&(prev->ucontext), &(next->ucontext));
 }
 
 
@@ -280,39 +285,27 @@ void uthread_init(uthread_t* uthread, void (*run_func)())
 	// Initialize the running time.
 	struct timeval tv = { .tv_sec = 0, .tv_usec = 0 };
 	uthread->running_time = tv;
-
-	// Set as active.
-	uthread->active = true;
 }
 
 
 /**
  * This function is expected to be a `start_routine` for `clone()`
  *
- * This interprets the given void pointer as a pointer to a `ptrpair_t`. The `fst`
- * pointer is interpreted as a `kthread_t*` and the second pointer is interpreted
- * as a `uthread_t*`. This will free the object which it is given.
+ * This interprets the given void pointer as a pointer to a `kthread_t`.
  */
 int kthread_runner(void* ptr)
 {
 	printf("kthread_runner()\n");
 
-	ptrpair_t* pair = ptr;
-	kthread_t* kt = pair->fst;
-	uthread_t* ut = pair->snd;
-	free(pair);
+	kthread_t* kt = ptr;
+	assert(kt != NULL);
+	assert(kt->running != NULL);
 
 	kt->tid = gettid();
-	kt->active = true;
+	kthread_timestamp(kt);
 
-	struct rusage ru;
-	const int RUSAGE_THREAD = 1;	// TODO: Fix this hack!
-	getrusage(RUSAGE_THREAD, &ru);  // Only available on linux.
-	kt->initial_utime = ru.ru_utime;
-	kt->initial_stime = ru.ru_stime;
-
-	ucontext_t* uc = &(ut->ucontext);
-	setcontext(uc);
+	// Switch to running the `uthread`.
+	setcontext(&(kt->running->ucontext));
 
 	assert(false);  // Execution should never reach here.
 }
@@ -331,7 +324,7 @@ kthread_t* kthread_self()
 	while (cur < end)
 	{
 		if (cur->tid == self_tid) {
-			assert(cur->active);  // TODO: make this more robust
+			assert(cur->running != NULL);  // TODO: make this more robust
 			return cur;
 		}
 		cur++;
@@ -347,18 +340,14 @@ kthread_t* kthread_self()
 int kthread_create(kthread_t* kt, uthread_t* ut)
 {
 	// TODO: everything!
-	assert(kt->active == false);
-
-	// It is the responsibility of the newly created thread to free `pair`.
-	ptrpair_t* pair = malloc(sizeof(ptrpair_t));
-	pair->fst = kt;
-	pair->snd = ut;
+	assert(kt->running == NULL);
+	kt->running = ut;
 
 	// Try using clone instead:
 	void *child_stack;
 	child_stack = (void *)malloc(CLONE_STACK_SIZE);
 	child_stack += CLONE_STACK_SIZE - 1;
-	int pid = clone(kthread_runner, child_stack, CLONE_VM|CLONE_FILES, pair);
+	int pid = clone(kthread_runner, child_stack, CLONE_VM|CLONE_FILES, kt);
 	assert(pid > 0);
 	return pid;
 }
@@ -368,11 +357,6 @@ int kthread_create(kthread_t* kt, uthread_t* ut)
 
 /* Define minor helper functions. ************************************************/
 
-void kthread_init(kthread_t* kt) {
-	kt->active = false;
-}
-
-
 /**
  * Returns a pointer to a `kthread_t` slot which is which is not active. If
  * no such `kthread_t` slot exists, then `NULL` is returned.
@@ -381,7 +365,7 @@ kthread_t* find_inactive_kthread()
 {
 	kthread_t* kthread = NULL;
 	for (int idx = 0; idx < _max_num_kthreads; idx++) {
-		if (_kthreads[idx].active == false) {
+		if (_kthreads[idx].running == NULL) {
 			kthread = _kthreads + idx;
 			break;
 		}
@@ -390,20 +374,13 @@ kthread_t* find_inactive_kthread()
 }
 
 
-/**
- * Returns a pointer to a `uthread_t` slot which is which is not active. If
- * no such `uthread_t` slot exists, then `NULL` is returned.
- */
-uthread_t* find_inactive_uthread()
+void kthread_timestamp(kthread_t* kt)
 {
-	uthread_t* uthread = NULL;
-	for (int idx = 0; idx < MAX_NUM_UTHREADS; idx++) {
-		if (_uthreads[idx].active == false) {
-			uthread = _uthreads + idx;
-			break;
-		}
-	}
-	return uthread;
+	struct rusage ru;
+	const int RUSAGE_THREAD = 1;	// TODO: Fix this hack!
+	getrusage(RUSAGE_THREAD, &ru);  // Only available on linux.
+	kt->initial_utime = ru.ru_utime;
+	kt->initial_stime = ru.ru_stime;
 }
 
 
